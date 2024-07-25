d4cf6ac9ef831fc702033dacfb296ac0d25145e2
ingiestein <ingie.stein@gmail.com>
2024-07-25 17:13:50 -0600

    7.25.24

--- lua/LDSLibrary/init.lua
+++ lua/LDSLibrary/init.lua
@@ -6,17 +6,21 @@ local M = {}
 
 ---@param opts? table
 M.setup = function(opts)
-	opts = opts or { language = "eng",
-					debub = false }
+	opts = opts or { language = "eng", debub = false }
 
-	local function myfunc(args)
+	local function scripturequery(args)
 		local input = args.args
 		local queries = parser.parse(input, opts)
 		scriptquery.scripturequery(queries, opts)
 	end
-
+	local function confquery(args)
+		local inputs = args.args
+		-- local querys = parser.parse(input, opts)
+		-- confquery.confquery(queries,opts)
+	end
 	local function load()
-		vim.api.nvim_create_user_command("MC", myfunc, { nargs = 1 })
+		vim.api.nvim_create_user_command("script", scripturequery, { nargs = 1 })
+		vim.api.nvim_create_user_command("conf", confquery, { nargs = 1 })
 	end
 	load()
 end
